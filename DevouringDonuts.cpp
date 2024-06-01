d9a9b8ca8ad921bbd24754020846ab271787bb13
Riya Ghosh <153933952+Riya-ghosh-sde@users.noreply.github.com>
2024-06-01 19:21:32 +0530

    Create DevouringDonuts.cpp

---
+++ DevouringDonuts.cpp
@@ -0,0 +1,18 @@
+//Starters 133 Division 4 (Rated)
+//platforme:-codechef
+//Q.name:-Devouring Donuts
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    int Y;
+    cin >> X >> Y; 
+    
+    int Total_Calories = X * Y; 
+    
+    cout << Total_Calories << endl; 
+    return 0;
+}
